1aec54863e537329532ab4a227b0997614593965
Santhiya <santhiyasaravanan41@gmail.com>
2025-03-03 21:21:18 +0400

    personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,18 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/name')
+def name():
+    return "Santhiya"
+
+@app.route('/register_number')
+def register_number():
+    return "22IT044"
+
+@app.route('/department')
+def department():
+    return "Information Technology"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
