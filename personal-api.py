d20e82c9b819dddff274661ca6d909b3ef4555b0
Santhiya <santhiyasaravanan41@gmail.com>
2025-03-08 22:27:13 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,18 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/name", methods=["GET"])
+def get_name():
+    return jsonify({"name": "Najib shahul hameed"})
+
+@app.route("/register_number", methods=["GET"])
+def get_register_number():
+    return jsonify({"register_number": "22IT025"})
+
+@app.route("/department", methods=["GET"])
+def get_department():
+    return jsonify({"department": "INFORMATION TECHNOLOGY"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
